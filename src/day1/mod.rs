a1188419473492216ba713693328b10001a7a3d5
Nathan Johnson <nathanjohnson@nathans-air.lan>
2022-11-30 22:10:56 -0500

    Initial commit

---
+++ src/day1/mod.rs
@@ -0,0 +1,4 @@
+pub fn part1() {
+    let input = super::files::read_lines("src/day1/input.txt");
+    println!("{:?}", input);
+}
---
+++ src/files/mod.rs
@@ -0,0 +1,14 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+pub fn read_lines(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
