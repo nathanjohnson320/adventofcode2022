c8e823b593cd8a7c6759a63a9843f2a55609dbd3
Nathan Johnson <nathanjohnson@nathans-air.lan>
2022-12-01 08:48:55 -0500

    Day 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,4 +1,64 @@
+use std::collections::HashMap;
+
 pub fn part1() {
+    let mut elf_index: i64 = 0;
+    let mut elf_calories: HashMap<i64, Vec<i64>> = HashMap::new();
+    elf_calories.insert(elf_index, Vec::new());
+
     let input = super::files::read_lines("src/day1/input.txt");
-    println!("{:?}", input);
+
+    // Could do this in one loop but the next one probably requires
+    // some other transformation so I'm separating them
+    for e in input.iter() {
+        if e == "" {
+            // New elf new vec
+            elf_index += 1;
+            elf_calories.insert(elf_index, Vec::new());
+
+            continue;
+        }
+
+        let calories = e.parse::<i64>().unwrap();
+        elf_calories.get_mut(&elf_index).unwrap().push(calories);
+    }
+
+    let mut max = 0;
+    for (_elf, balances) in elf_calories.iter() {
+        let sum: i64 = balances.iter().sum();
+        if sum > max {
+            max = sum;
+        }
+    }
+
+    println!("Part1 Max: {}", max);
+}
+
+pub fn part2() {
+    let mut elf_index: i64 = 0;
+    let mut elf_calories: HashMap<i64, Vec<i64>> = HashMap::new();
+    elf_calories.insert(elf_index, Vec::new());
+
+    let input = super::files::read_lines("src/day1/input.txt");
+
+    for e in input.iter() {
+        if e == "" {
+            // New elf new vec
+            elf_index += 1;
+            elf_calories.insert(elf_index, Vec::new());
+
+            continue;
+        }
+
+        let calories = e.parse::<i64>().unwrap();
+        elf_calories.get_mut(&elf_index).unwrap().push(calories);
+    }
+
+    let mut sums = Vec::new();
+    for (_elf, balances) in elf_calories.iter() {
+        let sum: i64 = balances.iter().sum();
+        sums.push(sum);
+    }
+    sums.sort();
+    let sum: i64 = sums.iter().rev().take(3).sum();
+    println!("Part1 Max: {}", sum);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod files;
 
 fn main() {
     day1::part1();
+    day1::part2();
 }
